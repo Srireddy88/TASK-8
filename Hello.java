591c9eddff6fc78ae1dc2fd07131783811ba859f
Srireddy88 <lakshmisri0805@gmail.com>
2025-04-18 16:04:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins + Maven!");
+        System.out.println("Hello Guys, Jenkins + Maven!");
     }
 }
